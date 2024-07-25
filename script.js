4cb255dcb601cf8e13755ff2e7774256f678fb82
fraubentz <140623725+fraubentz@users.noreply.github.com>
2024-07-25 19:52:43 +0100

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const qrImage = document.getElementById("qr-image");
-    qrImage.src = "path_to_your_qr_code_image.png"; // Replace with your QR code image path
-});
