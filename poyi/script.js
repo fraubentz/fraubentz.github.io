8e2810a8e3227b0af9c495e03f1202ca70af8b7b
fraubentz <140623725+fraubentz@users.noreply.github.com>
2024-07-25 19:46:06 +0100

    Add files via upload

---
+++ poyi/script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const qrImage = document.getElementById("qr-image");
+    qrImage.src = "portraitpoyibentz.png"; // Replace with your QR code image path
+});
