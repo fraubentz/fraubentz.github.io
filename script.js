c88f01fd2db42ecf34a3bd51a8ba832819c6654c
fraubentz <140623725+fraubentz@users.noreply.github.com>
2024-07-26 08:59:05 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const qrImage = document.getElementById("image");
+    qrImage.src = "codinginthekitchen.jpeg"; // Replace with your QR code image path
+});
